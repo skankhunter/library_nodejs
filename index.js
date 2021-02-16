21a3f3652cc5f242db03c35c9abf464e88a0ae89
Max Khoptyar <workpc@Maxs-MacBook-Pro.local>
2021-02-16 21:21:48 +0300

    Добавил роуты и multer

--- index.js
+++ index.js
@@ -1,98 +1,30 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const formData = require("express-form-data");
+const router = express.Router();
 
-const {Book} = require('./models');
-const {API} = require('./constants');
+const loggerMiddleware = require("./middleware/logger");
+const errorMiddleware = require("./middleware/routeError");
+const booksRouter = require("./routes/booksRoute");
+const authRouter = require("./routes/authRoute");
 
-const store = {
-    books: [],
-};
-
-[1, 2, 3].map(el => {
-    const book = new Book(`Title ${el}`, `Description ${el}`);
-    store.books.push(book);
-});
+const { API } = require("./constants");
 
 const app = express();
 
 app.use(formData.parse());
 app.use(cors());
+app.use(loggerMiddleware);
 
-app.get(`${API}/user/login`, (req, res) => {
-    res
-        .status(201)
-        .json({ id: 1, mail: "test@mail.ru" });
-});
+app.use('/public', express.static(__dirname+"/public"));
 
-app.get(`${API}/books`, (req, res) => {
-    const {books} = store;
-    res.json(books);
-});
+app.use(`${API}/user`, authRouter);
+app.use(`${API}/books`, booksRouter);
 
-app.get(`${API}/books/:id`, (req, res) => {
-    const {books} = store;
-    const {id} = req.params;
-    const book = books.find(book => book.id === id);
-
-    if (book) {
-        res.json(book);
-    } else {
-        res
-            .status(404)
-            .json("Not found");
-    }
-});
-
-app.post(`${API}/books`, (req, res) => {
-    const {books} = store;
-    const {title, description} = req.body;
-
-    const newBook = new Book(title, description);
-    books.push(newBook);
-
-    res
-        .status(201)
-        .json(newBook);
-});
-
-app.put(`${API}/books/:id`, (req, res) => {
-    const {books} = store;
-    const {title, description} = req.body;
-    const {id} = req.params;
-    const bookIndex = books.findIndex(book => book.id === id);
-
-    if (bookIndex !== -1) {
-        books[bookIndex] = {
-            ...books[bookIndex],
-            title,
-            description,
-        };
-        res.json(books[bookIndex]);
-    } else {
-        res
-            .status(404)
-            .json("Not found");
-    }
-});
-
-app.delete(`${API}/books/:id`, (req, res) => {
-    const {books} = store;
-    const {id} = req.params;
-    const bookIndex = books.findIndex(book => book.id === id);
-
-    if (bookIndex !== -1) {
-        books.splice(bookIndex, 1);
-        res.json('ok');
-    } else {
-        res
-            .status(404)
-            .json("todo | not found");
-    }
-});
+app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+   console.log(`Server is running on port ${PORT}`);
 });
---
+++ routes/booksRoute.js
@@ -0,0 +1,91 @@
+const { Router } = require("express");
+const { Book } = require("../models");
+const fileMiddleware = require("../middleware/file");
+
+const router = Router();
+
+const store = {
+   books: [],
+};
+
+[1, 2, 3].map((el) => {
+   const book = new Book(`Title ${el}`, `Description ${el}`);
+   store.books.push(book);
+});
+
+router.get(`/`, (req, res) => {
+   const { books } = store;
+   res.json(books);
+});
+
+router.get(`/:id`, (req, res) => {
+   const { books } = store;
+   const { id } = req.params;
+   const book = books.find((book) => book.id === id);
+
+   if (book) {
+      res.json(book);
+   } else {
+      res.status(404).json("Not found");
+   }
+});
+
+router.get(`/:id/download`, (req, res) => {
+   const { books } = store;
+   const { id } = req.params;
+   const book = books.find((book) => book.id === id);
+
+   res.download(__dirname + book.fileBook, book.fileName, (err) => {
+      if (err) {
+         res.status(404).json();
+      }
+   });
+});
+
+router.post(`/`, fileMiddleware.single("book-pdf"), (req, res) => {
+   const { books } = store;
+   const { title, description } = req.body;
+
+   if (req.file) {
+      const { filename, path } = req.file;
+
+      const newBook = new Book(title, description, "", "", "", filename, path);
+      books.push(newBook);
+      res.status(201).json(newBook);
+   } else {
+      res.status(404).json();
+   }
+});
+
+router.put(`/:id`, (req, res) => {
+   const { books } = store;
+   const { title, description } = req.body;
+   const { id } = req.params;
+   const bookIndex = books.findIndex((book) => book.id === id);
+
+   if (bookIndex !== -1) {
+      books[bookIndex] = {
+         ...books[bookIndex],
+         title,
+         description,
+      };
+      res.json(books[bookIndex]);
+   } else {
+      res.status(404).json("Not found");
+   }
+});
+
+router.delete(`/:id`, (req, res) => {
+   const { books } = store;
+   const { id } = req.params;
+   const bookIndex = books.findIndex((book) => book.id === id);
+
+   if (bookIndex !== -1) {
+      books.splice(bookIndex, 1);
+      res.json("ok");
+   } else {
+      res.status(404).json("todo | not found");
+   }
+});
+
+module.exports = router;
