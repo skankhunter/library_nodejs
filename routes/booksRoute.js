2aae3b3b6be0b6a4f71676004654327c86e6838e
Max Khoptyar <mkhoptyar@alfabank.ru>
2021-02-23 15:04:47 +0300

    Добавил вьюхи создание книги без файла

--- routes/booksRoute.js
+++ routes/booksRoute.js
@@ -16,7 +16,11 @@ const store = {
 
 router.get(`/`, (req, res) => {
    const { books } = store;
-   res.json(books);
+   res.render("books/index", { books, title: "Books list" });
+});
+
+router.get(`/create`, (req, res) => {
+   res.render("books/create", { book: {}, title: "Creation" });
 });
 
 router.get(`/:id`, (req, res) => {
@@ -24,11 +28,9 @@ router.get(`/:id`, (req, res) => {
    const { id } = req.params;
    const book = books.find((book) => book.id === id);
 
-   if (book) {
-      res.json(book);
-   } else {
-      res.status(404).json("Not found");
-   }
+   return book
+      ? res.render("books/view", { book, title: "Просмотр книги" })
+      : res.redirect("/404");
 });
 
 router.get(`/:id/download`, (req, res) => {
@@ -39,54 +41,82 @@ router.get(`/:id/download`, (req, res) => {
 
    res.download(pathToFile, book.fileName, (err) => {
       if (err) {
-         res.status(404).json();
+         res.status(404).redirect(`/404`);
       }
    });
 });
 
-router.post(`/`, fileMiddleware.single("book-pdf"), (req, res) => {
+router.post(`/create`, fileMiddleware.single("book-pdf"), (req, res) => {
    const { books } = store;
-   const { title, description } = req.body;
+   const { title, description, authors } = req.body;
 
-   if (req.file) {
-      const { filename, path } = req.file;
+   const newBook = new Book(
+      title,
+      description,
+      authors,
+      "",
+      "",
+      "filename",
+      path
+   );
+   books.push(newBook);
+   res.redirect("/books");
 
-      const newBook = new Book(title, description, "", "", "", filename, path);
-      books.push(newBook);
-      res.status(201).json(newBook);
+   // if (req.file) {
+   //    const { filename, path } = req.file;
+
+   //    const newBook = new Book(title, description, "", "", "", filename, path);
+   //    books.push(newBook);
+   //    res.status(201).redirect("books/index", { books });
+   // } else {
+   //    res.status(404).redirect(`/error/404`);
+   // }
+});
+
+router.get("/update/:id", (req, res) => {
+   const { books } = store;
+   const { id } = req.params;
+   const bookIndex = books.findIndex((book) => book.id == id);
+
+   if (bookIndex !== -1) {
+      res.render("books/update", {
+         title: "Book | view",
+         book: books[bookIndex],
+      });
    } else {
-      res.status(404).json();
+      res.status(404).redirect("/404");
    }
 });
 
-router.put(`/:id`, (req, res) => {
+router.post(`/update/:id`, (req, res) => {
    const { books } = store;
-   const { title, description } = req.body;
+   const { title, description, authors } = req.body;
    const { id } = req.params;
    const bookIndex = books.findIndex((book) => book.id == id);
-   
+
    if (bookIndex !== -1) {
       books[bookIndex] = {
          ...books[bookIndex],
          title,
          description,
+         authors,
       };
-      res.json(books[bookIndex]);
+      res.redirect(`/books/${id}`);
    } else {
-      res.status(404).json("Not found");
+      res.status(404).redirect(`/404`);
    }
 });
 
-router.delete(`/:id`, (req, res) => {
+router.post(`/delete/:id`, (req, res) => {
    const { books } = store;
    const { id } = req.params;
    const bookIndex = books.findIndex((book) => book.id == id);
 
    if (bookIndex !== -1) {
       books.splice(bookIndex, 1);
-      res.json("ok");
+      res.redirect(`/books`);
    } else {
-      res.status(404).json("todo | not found");
+      res.status(404).redirect(`/404`);
    }
 });
 
