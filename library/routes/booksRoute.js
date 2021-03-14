2fe79b15376c378a3b846b4ffb23364e81d1dacd
Max Khoptyar <mkhoptyar@alfabank.ru>
2021-03-14 11:07:43 +0300

    Добавлена работа Mongoose + MongoDB

--- library/routes/booksRoute.js
+++ library/routes/booksRoute.js
@@ -1,14 +1,12 @@
 const { Router } = require("express");
 const path = require("path");
+const fs = require("fs");
 const fetch = require("node-fetch");
-const { Book } = require("../models");
 const fileMiddleware = require("../middleware/file");
+const Book = require("../models/bookSchema");
+const { getFileData } = require("../utils");
 
 const router = Router();
-
-const store = {
-   books: [],
-};
 
 const getViews = async (id) => {
    return await fetch(`http://counter:3000/counter/${id}`).then((data) =>
@@ -22,146 +20,149 @@ const incrViews = async (id) => {
    });
 };
 
-[1, 2, 3].map((el) => {
-   const book = new Book(`Title ${el}`, `Description ${el}`);
-   store.books.push(book);
-});
+router.get(`/`, async (req, res) => {
+   const books = await Book.find();
 
-router.get(`/`, (req, res) => {
-   const { books } = store;
    res.render("books/index", { books, title: "Books list" });
 });
 
 router.get(`/create`, (req, res) => {
-   res.render("books/create", { book: {}, title: "Creation" });
+   const defaultRenderBook = {
+      fileCover: {
+         data: "",
+         contentType: "",
+      },
+   };
+   res.render("books/create", { book: defaultRenderBook, title: "Creation" });
 });
 
 router.get(`/:id`, async (req, res) => {
-   const { books } = store;
    const { id } = req.params;
-   const book = books.find((book) => book.id === id);
 
-   if (book) {
-      try {
+   try {
+      const book = await Book.findById(id);
+      if (book) {
          const bookCounter = await getViews(id);
          await incrViews(id);
+         book.views = bookCounter;
 
          return res.render("books/view", {
-            book: { ...book, views: bookCounter },
+            book,
             title: "Просмотр книги",
          });
-      } catch {
-         return res.status(500).json("Ошибка сервиса счетчика");
+      } else {
+         return res.redirect("/404");
       }
-   } else {
-      return res.redirect("/404");
+   } catch {
+      console.error(e);
+      return res.status(500).json("Ошибка сервиса счетчика");
    }
 });
 
-router.get(`/:id/download`, (req, res) => {
-   const { books } = store;
+router.get(`/:id/download`, async (req, res) => {
    const { id } = req.params;
-   const book = books.find((book) => book.id === id);
-   const pathToFile = path.join(__dirname, `../${book.fileBook}`);
 
-   res.download(pathToFile, book.fileName, (err) => {
-      if (err) {
-         res.redirect(`/404`);
-      }
-   });
+   try {
+      const book = await Book.findById(id);
+
+      const pathToFile = path.join(__dirname, `../${book.fileBook}`);
+      res.download(pathToFile, book.fileName, (err) => {
+         if (err) res.redirect(`/404`);
+      });
+   } catch (e) {
+      console.error(e);
+      res.redirect(`/404`);
+   }
 });
 
 router.post(
    `/create`,
    fileMiddleware.fields([
-      { name: "image-cover", maxCount: 1 },
-      { name: "book-pdf", maxCount: 1 },
+      { name: "imgFiles", maxCount: 1 },
+      { name: "booksFiles", maxCount: 1 },
    ]),
-   (req, res) => {
-      const { books } = store;
+   async (req, res) => {
       const { title, description, authors } = req.body;
 
-      if (req.files) {
-         const imgCover =
-            req.files["image-cover"] && req.files["image-cover"][0];
-         const bookPdf = req.files["book-pdf"] && req.files["book-pdf"][0];
+      try {
+         const fileCover = getFileData(req.files, "imgFiles");
+         const fileBook = getFileData(req.files, "booksFiles");
 
-         const newBook = new Book(
+         const newBook = new Book({
             title,
             description,
             authors,
-            "",
-            imgCover && imgCover.path,
-            bookPdf && bookPdf.filename,
-            bookPdf && bookPdf.path
-         );
+            favorite: false,
+            fileCover,
+            fileName: fileBook.filename,
+            fileBook,
+         });
 
-         books.push(newBook);
+         await newBook.save();
          res.redirect("/books");
-      } else {
+      } catch (e) {
+         console.log(e);
          res.redirect(`/error/404`);
       }
    }
 );
 
-router.get("/update/:id", (req, res) => {
-   const { books } = store;
+router.get("/update/:id", async (req, res) => {
    const { id } = req.params;
-   const bookIndex = books.findIndex((book) => book.id == id);
 
-   if (bookIndex !== -1) {
-      res.render("books/update", {
-         title: "Book | view",
-         book: books[bookIndex],
-      });
-   } else {
-      res.redirect("/404");
+   try {
+      const book = await Book.findById(id);
+      return book
+         ? res.render("books/update", {
+              title: "Book | view",
+              book,
+           })
+         : res.redirect("/404");
+   } catch (e) {
+      console.error(e);
+      res.status(404).redirect("/404");
    }
 });
 
 router.post(
    `/update/:id`,
    fileMiddleware.fields([
-      { name: "image-cover", maxCount: 1 },
-      { name: "book-pdf", maxCount: 1 },
+      { name: "imgFiles", maxCount: 1 },
+      { name: "booksFiles", maxCount: 1 },
    ]),
-   (req, res) => {
-      const { books } = store;
+   async (req, res) => {
       const { title, description, authors } = req.body;
       const { id } = req.params;
-      const bookIndex = books.findIndex((book) => book.id == id);
 
-      if (bookIndex !== -1 && req.files) {
-         const imgCover =
-            req.files["image-cover"] && req.files["image-cover"][0];
-         const bookPdf = req.files["book-pdf"] && req.files["book-pdf"][0];
+      try {
+         const fileCover = getFileData(req.files, "imgFiles");
+         const fileBook = getFileData(req.files, "booksFiles");
 
-         books[bookIndex] = {
-            ...books[bookIndex],
+         await Book.findByIdAndUpdate(id, {
             title,
             description,
             authors,
-            fileCover: imgCover && imgCover.path,
-            fileName: bookPdf && bookPdf.filename,
-            fileBook: bookPdf && bookPdf.path,
-         };
+            fileCover,
+            fileName: fileBook.filename,
+            fileBook,
+         });
 
          res.redirect(`/books/${id}`);
-      } else {
-         res.redirect(`/error/404`);
+      } catch (e) {
+         console.error(e);
+         res.status(404).redirect("/404");
       }
    }
 );
 
-router.post(`/delete/:id`, (req, res) => {
-   const { books } = store;
+router.post(`/delete/:id`, async (req, res) => {
    const { id } = req.params;
-   const bookIndex = books.findIndex((book) => book.id == id);
 
-   if (bookIndex !== -1) {
-      books.splice(bookIndex, 1);
+   try {
+      await Book.deleteOne({ _id: id });
       res.redirect(`/books`);
-   } else {
+   } catch (e) {
+      console.log(e);
       res.redirect(`/404`);
    }
 });
